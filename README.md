67f0be66ab8f9ea7cac476be82fb923adf3ba607
Dimitri Koussa <dimitri.koussa@gmail.com>
2014-01-09 06:46:30 +1100

    Removed the set user functionality

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 git-username-app-indicator
 ==========================
 
-Shows the git user.name using AppIndicator and provides a context menu to pick the last few options.
+Shows the git user.name using AppIndicator.
 
 This is useful in a pair-programming development environment where:
 * the user.name is set to the name of the pair (e.g., 'John Smith and Bob Jones')
 * you swap machines when you swap pairs
 * the git user.name setting is cleared every day
-This tool makes the current value visible on the panel and provides a convenience menu to assign a new value.
+This tool makes the current value visible on the panel.
 
-This tool was developed as a quality of life enhancement for software developers at Tyro Payments.
+This tool was developed as a quality of life enhancement for software developers at Tyro.
 
 Git-Icon-White.png from http://git-scm.com/downloads/logos
--- git-username-app-indicator.py
+++ git-username-app-indicator.py
@@ -7,8 +7,6 @@ from gi.repository import Gtk, GLib
 import commands
 import sys
 import os
-import pygtk
-pygtk.require('2.0')
 
 try: 
        from gi.repository import AppIndicator3 as AppIndicator  
@@ -17,11 +15,11 @@ except:
 
 import re
 
-class IndicatorCPUSpeed:
+class GitUsernameAppIndicator:
     def __init__(self):
         image_path = os.path.abspath(os.path.dirname(sys.argv[0])) + "/Git-Icon-White.png"
         self.ind = AppIndicator.Indicator.new(
-                            "indicator-cpuspeed", 
+                            "git-username-app-indicator",
                             image_path,
                             AppIndicator.IndicatorCategory.APPLICATION_STATUS);
         self.ind.set_status (AppIndicator.IndicatorStatus.ACTIVE)
@@ -29,16 +27,7 @@ class IndicatorCPUSpeed:
         self.menu = Gtk.Menu()
 
         item = Gtk.MenuItem()
-        item.set_label("Enter git su...")
-        item.connect("activate", self.pop_up_text_entry)
-        item.show()
-        self.menu.append(item)
-
-        self.menu.append(Gtk.SeparatorMenuItem())
-
-        item = Gtk.MenuItem()
-        item.set_label("Yesterday's developers")
-        item.connect("activate", self.pop_up_text_entry)
+        item.set_label("This menu item does nothing but is required")
         item.show()
         self.menu.append(item)
 
@@ -50,32 +39,6 @@ class IndicatorCPUSpeed:
 
     def get_git_su(self):
         return commands.getstatusoutput("git config user.name")[1];
-
-    def handler_menu_exit(self, evt):
-        Gtk.main_quit()
-
-    def handle_text_entry(self):
-        text = entry.get_text()
-        print('text: ' + text)
-
-    def pop_up_text_entry(self, evt):
-        window = Gtk.Window(Gtk.WINDOW_TOPLEVEL)
-        window.set_size_request(200, 100)
-        window.set_title("GTK Entry")
-        window.connect("delete_event", lambda w,e: Gtk.main_quit())
-
-        vbox = Gtk.VBox(False, 0)
-        window.add(vbox)
-        vbox.show()
-
-        entry = Gtk.Entry()
-        entry.set_max_length(50)
-        entry.connect("activate", self.handle_text_entry, entry)
-        entry.set_text("hello")
-        entry.insert_text(" world", len(entry.get_text()))
-        entry.select_region(0, len(entry.get_text()))
-        vbox.pack_start(entry, True, True, 0)
-        entry.show()
 
     def handler_timeout(self):
         self.update_git_su_status()
@@ -91,5 +54,5 @@ if __name__ == "__main__":
     import signal
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-    ind = IndicatorCPUSpeed()
+    ind = GitUsernameAppIndicator()
     ind.main()
